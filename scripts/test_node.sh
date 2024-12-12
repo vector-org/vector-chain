650425d7d773fe5e86a60902265f2aa6efdd2be5
kien6034 <trinhtrungkientb99@gmail.com>
2024-12-13 02:46:31 +0700

    fix testnode script

--- scripts/test_node.sh
+++ scripts/test_node.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Run this script to quickly install, setup, and run the current version of the network without docker.
-#
-# Examples:
-# CHAIN_ID="localchain-1" HOME_DIR="~/.vector" BLOCK_TIME="1000ms" CLEAN=true sh scripts/test_node.sh
-# CHAIN_ID="localchain-2" HOME_DIR="~/.vector" CLEAN=true RPC=36657 REST=2317 PROFF=6061 P2P=36656 GRPC=8090 GRPC_WEB=8091 ROSETTA=8081 BLOCK_TIME="500ms" sh scripts/test_node.sh
 
 export KEY="acc0"
 export KEY2="acc1"
@@ -25,63 +20,49 @@ export GRPC=${GRPC:-"9090"}
 export GRPC_WEB=${GRPC_WEB:-"9091"}
 export PROFF_LADDER=${PROFF_LADDER:-"6060"}
 export ROSETTA=${ROSETTA:-"8080"}
-export BLOCK_TIME=${BLOCK_TIME:-"5s"}
+export BLOCK_TIME=${BLOCK_TIME:-"1s"}
 
-# if which binary does not exist, install it
+# Ensure binary exists
 if [ -z `which $BINARY` ]; then
   make install
-
   if [ -z `which $BINARY` ]; then
     echo "Ensure $BINARY is installed and in your PATH"
     exit 1
   fi
 fi
 
-alias BINARY="$BINARY --home=$HOME_DIR"
-
 command -v $BINARY > /dev/null 2>&1 || { echo >&2 "$BINARY command not found. Ensure this is setup / properly installed in your GOPATH (make install)."; exit 1; }
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
 set_config() {
-  $BINARY config set client chain-id $CHAIN_ID --home=$HOME_DIR
-  $BINARY config set client keyring-backend $KEYRING --home=$HOME_DIR
+  $BINARY config set client chain-id $CHAIN_ID --home $HOME_DIR
+  $BINARY config set client keyring-backend $KEYRING --home $HOME_DIR
 }
-set_config
 
-
-from_scratch () {
-  # Fresh install on current branch
-  make install
-
-  # remove existing daemon files.
+from_scratch() {
+  echo "Creating new chain from scratch..."
+  
+  # remove existing daemon files
   if [ ${#HOME_DIR} -le 2 ]; then
-      echo "HOME_DIR must be more than 2 characters long"
-      return
+    echo "HOME_DIR must be more than 2 characters long"
+    return
   fi
-  rm -rf $HOME_DIR && echo "Removed $HOME_DIR"
+  rm -rf $HOME_DIR
+  
+  # Create chain
+  $BINARY init $MONIKER --chain-id $CHAIN_ID --default-denom $DENOM --home $HOME_DIR
 
-  # reset values if not set already after whipe
-  set_config
+  # Add keys
+  echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | \
+    $BINARY keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover --home $HOME_DIR
+  
+  echo "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise" | \
+    $BINARY keys add $KEY2 --keyring-backend $KEYRING --algo $KEYALGO --recover --home $HOME_DIR
 
-  add_key() {
-    key=$1
-    mnemonic=$2
-    echo $mnemonic | $BINARY keys add $key --keyring-backend $KEYRING --algo $KEYALGO --recover --home=$HOME_DIR
-  }
-
-  # vector1efd63aw40lxf3n4mhf7dzhjkr453axur6wlkdg
-  add_key $KEY "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry"
-  # vector1hj5fveer5cjtn4wd6wstzugjfdxzl0xpx6vjyp
-  add_key $KEY2 "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise"
-
-  # chain initial setup
-  $BINARY init $MONIKER --chain-id $CHAIN_ID --default-denom $DENOM --home=$HOME_DIR
-
+  # Update genesis
   update_test_genesis () {
     cat $HOME_DIR/config/genesis.json | jq "$1" > $HOME_DIR/config/tmp_genesis.json && mv $HOME_DIR/config/tmp_genesis.json $HOME_DIR/config/genesis.json
   }
-
-  # === CORE MODULES ===
 
   # Block
   update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
@@ -91,69 +72,55 @@ from_scratch () {
   update_test_genesis '.app_state["gov"]["params"]["voting_period"]="30s"'
   update_test_genesis '.app_state["gov"]["params"]["expedited_voting_period"]="15s"'
 
-  # staking
+  # Staking
   update_test_genesis `printf '.app_state["staking"]["params"]["bond_denom"]="%s"' $DENOM`
   update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.050000000000000000"'
 
-  # mint
+  # Mint
   update_test_genesis `printf '.app_state["mint"]["params"]["mint_denom"]="%s"' $DENOM`
 
-  # crisis
+  # Crisis
   update_test_genesis `printf '.app_state["crisis"]["constant_fee"]={"denom":"%s","amount":"1000"}' $DENOM`
 
-  # === CUSTOM MODULES ===
-  # tokenfactory
+  # Custom modules
   update_test_genesis '.app_state["tokenfactory"]["params"]["denom_creation_fee"]=[]'
   update_test_genesis '.app_state["tokenfactory"]["params"]["denom_creation_gas_consume"]=100000'
 
-  # Allocate genesis accounts
-  $BINARY genesis add-genesis-account $KEY 10000000$DENOM,900test --keyring-backend $KEYRING --append --home $HOME_DIR
-  $BINARY genesis add-genesis-account $KEY2 10000000$DENOM,800test --keyring-backend $KEYRING --append --home $HOME_DIR
+  # Add genesis accounts
+  $BINARY genesis add-genesis-account $KEY 10000000000000$DENOM --keyring-backend $KEYRING --home $HOME_DIR
+  $BINARY genesis add-genesis-account $KEY2 10000000000000$DENOM --keyring-backend $KEYRING --home $HOME_DIR
 
-  # Sign genesis transaction
-  $BINARY genesis gentx $KEY 1000000$DENOM --keyring-backend $KEYRING --chain-id $CHAIN_ID --home $HOME_DIR
+  # Generate genesis tx
+  $BINARY genesis gentx $KEY 1000000000000$DENOM --keyring-backend $KEYRING --chain-id $CHAIN_ID --home $HOME_DIR
 
+  # Collect genesis tx
   $BINARY genesis collect-gentxs --home $HOME_DIR
 
+  # Validate genesis
   $BINARY genesis validate-genesis --home $HOME_DIR
-  err=$?
-  if [ $err -ne 0 ]; then
-    echo "Failed to validate genesis"
-    return
-  fi
 }
 
-# check if CLEAN is not set to false
-if [ "$CLEAN" != "false" ]; then
-  echo "Starting from a clean state"
-  
+# Initialize chain if CLEAN is true or if genesis doesn't exist
+if [ "$CLEAN" = "true" ] || [ ! -f "$HOME_DIR/config/genesis.json" ]; then
+  echo "Initializing chain..."
   from_scratch
+  set_config
 fi
 
 echo "Starting node..."
 
-# Opens the RPC endpoint to outside connections
-sed -i -e 's/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:'$RPC'"/g' $HOME_DIR/config/config.toml
-sed -i -e 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' $HOME_DIR/config/config.toml
+# Update configs
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${REST}\"%" $HOME_DIR/config/app.toml
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${GRPC}\"%" $HOME_DIR/config/app.toml
+sed -i.bak -e "s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${GRPC_WEB}\"%" $HOME_DIR/config/app.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:${RPC}\"%" $HOME_DIR/config/client.toml
+sed -i.bak -e "s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://0.0.0.0:${RPC}\"%" $HOME_DIR/config/config.toml
+sed -i.bak -e "s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${P2P}\"%" $HOME_DIR/config/config.toml
+sed -i.bak -e "s%^timeout_commit = \"5s\"%timeout_commit = \"${BLOCK_TIME}\"%" $HOME_DIR/config/config.toml
+sed -i.bak -e 's%^cors_allowed_origins = \[\]%cors_allowed_origins = \["*"\]%' $HOME_DIR/config/config.toml
 
-# REST endpoint
-sed -i -e 's/address = "tcp:\/\/localhost:1317"/address = "tcp:\/\/0.0.0.0:'$REST'"/g' $HOME_DIR/config/app.toml
-sed -i -e 's/enable = false/enable = true/g' $HOME_DIR/config/app.toml
-sed -i -e 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' $HOME_DIR/config/app.toml
-
-# peer exchange
-sed -i -e 's/pprof_laddr = "localhost:6060"/pprof_laddr = "localhost:'$PROFF'"/g' $HOME_DIR/config/config.toml
-sed -i -e 's/laddr = "tcp:\/\/0.0.0.0:26656"/laddr = "tcp:\/\/0.0.0.0:'$P2P'"/g' $HOME_DIR/config/config.toml
-
-# GRPC
-sed -i -e 's/address = "localhost:9090"/address = "0.0.0.0:'$GRPC'"/g' $HOME_DIR/config/app.toml
-sed -i -e 's/address = "localhost:9091"/address = "0.0.0.0:'$GRPC_WEB'"/g' $HOME_DIR/config/app.toml
-
-# Rosetta Api
-sed -i -e 's/address = ":8080"/address = "0.0.0.0:'$ROSETTA'"/g' $HOME_DIR/config/app.toml
-
-# Faster blocks
-sed -i -e 's/timeout_commit = "5s"/timeout_commit = "'$BLOCK_TIME'"/g' $HOME_DIR/config/config.toml
-
-# Start the node with 0 gas fees
-$BINARY start --pruning=nothing  --minimum-gas-prices=0$DENOM --rpc.laddr="tcp://0.0.0.0:$RPC" --home=$HOME_DIR
+# Start the node
+$BINARY start --home $HOME_DIR \
+  --pruning=nothing \
+  --minimum-gas-prices=0$DENOM \
+  --rpc.laddr="tcp://0.0.0.0:$RPC"
---
+++ scripts/upgrade_test.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+# the upgrade is a fork, "true" otherwise
+FORK=${FORK:-"false"}
+
+OLD_VERSION=v1.0.0
+UPGRADE_WAIT=${UPGRADE_WAIT:-20}
+HOME=mytestnet
+ROOT=$(pwd)
+DENOM=uvctr
+CHAIN_ID=localvector
+SOFTWARE_UPGRADE_NAME="v0.2.0"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$FORK" == "true" ]]; then
+    export VECTOR_HALT_HEIGHT=20
+fi
+
+
+# remove home dir
+echo "Cleaning up..."
+pkill vectord
+rm -rf $HOME
+mkdir -p $HOME
+
+# underscore so that go tool will not take gocache into account
+mkdir -p _build/gocache
+export GOMODCACHE=$ROOT/_build/gocache
+
+echo "Setting up old version..."
+# Build old version
+if [ $# -eq 1 ] && [ "$1" = "--reinstall-old" ] || ! command -v _build/old/vectord &> /dev/null; then
+    echo "Building old version ($OLD_VERSION)..."
+    mkdir -p _build/old
+    
+    # Store current branch
+    echo "Current branch: $git_current_branch"
+    git_current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+    echo "Checking out old version ($OLD_VERSION)"
+    git checkout -q $OLD_VERSION
+    git branch --show-current
+
+    echo "Building old binary"
+    GOBIN="$ROOT/_build/old" go install -mod=readonly ./...
+    
+    # Return to original branch
+    echo "Returning to original branch ($git_current_branch)"
+    git checkout -q $git_current_branch
+    
+    echo "Old binary built successfully"
+fi
+
+echo "Setting up new version..." 
+# Build new version (current branch)
+if ! command -v _build/new/vectord &> /dev/null
+then
+    echo "Building new version (current branch)"
+    git checkout $CURRENT_BRANCH
+    git branch --show-current
+    mkdir -p _build/new
+    GOBIN="$ROOT/_build/new" go install -mod=readonly ./...
+fi
+
+echo "Running old node..."
+# run old node
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Running on macOS"
+    screen -L -dmS node1 bash scripts/test_node.sh _build/old/vectord $DENOM --Logfile $HOME/log-screen.txt
+else
+    echo "Running on Linux"
+    screen -L -Logfile $HOME/log-screen.txt -dmS node1 bash scripts/test_node.sh _build/old/vectord $DENOM
+fi
+
+sleep 5
+
+
+run_fork () {
+    echo "forking"
+
+    while true; do
+        BLOCK_HEIGHT=$(./_build/old/vectord status | jq '.SyncInfo.latest_block_height' -r)
+        # if BLOCK_HEIGHT is not empty
+        if [ ! -z "$BLOCK_HEIGHT" ]; then
+            echo "BLOCK_HEIGHT = $BLOCK_HEIGHT"
+            sleep 10
+        else
+            echo "BLOCK_HEIGHT is empty, forking"
+            break
+        fi
+    done
+}
+
+run_upgrade () {
+    echo -e "\n\n=> =>start upgrading"
+
+    # Get upgrade height, 20 blocks after current height
+    STATUS_INFO=($(./_build/old/vectord status --home $HOME | jq -r '.sync_info.latest_block_height'))
+    UPGRADE_HEIGHT=$((STATUS_INFO + 20))
+    echo "UPGRADE_HEIGHT = $UPGRADE_HEIGHT"
+
+    tar -cf ./_build/new/vectord.tar -C ./_build/new vectord
+    SUM=$(shasum -a 256 ./_build/new/vectord.tar | cut -d ' ' -f1)
+
+    # Create proposal json
+    cat > proposal.json << EOF
+{
+    "messages": [
+        {
+            "@type": "/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade",
+            "authority": "vector10d07y265gmmuvt4z0w9aw880jnsr700j53vrug",
+            "plan": {
+                "name": "$SOFTWARE_UPGRADE_NAME",
+                "time": "0001-01-01T00:00:00Z",
+                "height": "$UPGRADE_HEIGHT",
+                "info": "test",
+                "upgraded_client_state": null
+            }
+        }
+    ],
+    "metadata": "ipfs://CID",
+    "deposit": "200000${DENOM}",
+    "title": "Software Upgrade $SOFTWARE_UPGRADE_NAME",
+    "summary": "Upgrade to version $SOFTWARE_UPGRADE_NAME"
+}
+EOF
+
+    echo "submit upgrade"
+    echo "Proposal content:"
+    cat proposal.json
+
+    ./_build/old/vectord tx gov submit-proposal proposal.json \
+        --from=val1 \
+        --keyring-backend=test \
+        --chain-id=$CHAIN_ID \
+        --home=$HOME \
+        -y
+    sleep 3
+
+    echo "Deposit"
+    ./_build/old/vectord tx gov deposit 1 "10000000${DENOM}" \
+        --from val1 \
+        --keyring-backend test \
+        --chain-id $CHAIN_ID \
+        --home $HOME \
+        --gas 2000000 \
+        --fees 1000000$DENOM \
+        -y > /dev/null
+
+    sleep 2
+
+    echo "Vote proposal validator1"
+    ./_build/old/vectord tx gov vote 1 yes \
+        --from val1 \
+        --keyring-backend test \
+        --chain-id $CHAIN_ID \
+        --home $HOME \
+        --gas 2000000 \
+        --fees 1000000$DENOM \
+        -y > /dev/null
+
+    sleep 3
+
+    echo "Vote proposal validator2"
+    ./_build/old/vectord tx gov vote 1 yes \
+        --from val2 \
+        --keyring-backend test \
+        --chain-id $CHAIN_ID \
+        --home $HOME \
+        --gas 2000000 \
+        --fees 1000000$DENOM \
+        -y > /dev/null
+
+    sleep 5
+
+    # determine block_height to halt
+    while true; do
+        BLOCK_HEIGHT=$(./_build/old/vectord status | jq '.SyncInfo.latest_block_height' -r)
+        if [ $BLOCK_HEIGHT = "$UPGRADE_HEIGHT" ]; then
+            # assuming running only 1 vectord
+            echo "BLOCK HEIGHT = $UPGRADE_HEIGHT REACHED, KILLING OLD ONE"
+            pkill vectord
+            break
+        else
+            ./_build/old/vectord q gov proposal 1 --output=json | jq ".status"
+            echo "BLOCK_HEIGHT = $BLOCK_HEIGHT"
+            sleep 1
+        fi
+    done
+}
+
+
+# if FORK = true
+if [[ "$FORK" == "true" ]]; then
+    run_fork
+    unset VECTOR_HALT_HEIGHT
+else
+    run_upgrade
+fi
+
+sleep 1
+
+# run new node
+echo -e "\n\n=> =>continue running nodes after upgrade"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CONTINUE="true" screen -L -dmS node1 bash scripts/run-node.sh _build/new/vectord $DENOM
+else
+    CONTINUE="true" screen -L -dmS node1 bash scripts/run-node.sh _build/new/vectord $DENOM
+fi
+
+sleep 5
